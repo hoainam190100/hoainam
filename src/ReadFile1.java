a9cd038953dbee4bf438df4b837f725b55a3aa89
hoainam190100 <hoainam190194@gmail.com>
2020-01-20 15:49:10 +0700

    baitap 1

--- src/ReadFile1.java
+++ src/ReadFile1.java
@@ -1,9 +1,12 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
-public class ReadFile {
+public class ReadFile1 {
     public static void main(String[] args) {
         try{
-         File file =new File("/Users/admin/IdeaProjects/ônlạikiếmthuc/hello/test.CVS");
+         File file =new File("/Users/admin/IdeaProjects/ônlạikiếmthuc/hello/test1.CVS");
             FileReader fileReader =new FileReader(file);
             BufferedReader bufferedReader =new BufferedReader(fileReader);
             String line=null;
